319275ca4964c9de9ee927eea239fe51ac1912bd
jenerlov <jenerlov@gmail.com>
2023-06-09 12:47:40 +0200

    js

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,4 +1,21 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿function toggleMenu(attribute) {
+	try {
+		const toggleBtn = document.querySelector(attribute);
+		toggleBtn.addEventListener("click", function () {
+			const element = document.querySelector(
+				toggleBtn.getAttribute("data-target")
+			);
 
-// Write your JavaScript code.
+			if (!element.classList.contains("open-menu")) {
+				element.classList.add("open-menu");
+				toggleBtn.classList.add("btn-outline.dark");
+				toggleBtn.classList.add("btn-toggle.white");
+			} else {
+				element.classList.remove("open-menu");
+				toggleBtn.classList.remove("btn-outline-dark");
+				toggleBtn.classList.remove("btn-toggle-white");
+			}
+		});
+	} catch {}
+}
+toggleMenu("[data-option-toggle]");
